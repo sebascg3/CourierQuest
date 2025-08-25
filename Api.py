05d6f00319d4eb063acaa18013d7315688cd5e8a
Sebastian <sebasdg0308@gmail.com>
2025-08-24 21:31:23 -0600

    Mejora del API

--- Api.py
+++ Api.py
@@ -1,10 +1,23 @@
 import requests
-base_url = "https://tigerds-api.kindflower-ccaf48b6.eastus.azurecontainerapps.io"
-def get_map_data():
-    response = requests.get(f"{base_url}/city/map")
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return {f"error": f"Failed to retrieve map data, {response.status_code}"}
 
-print(get_map_data())
+BASE_URL = "https://tigerds-api.kindflower-ccaf48b6.eastus.azurecontainerapps.io"
+
+# Obtener el mapa de la ciudad
+map_response = requests.get(f"{BASE_URL}/city")
+map_data = map_response.json()["data"]
+
+getHealth = requests.get(f"{BASE_URL}/healthz")
+getCityName = requests.get(f"{BASE_URL}/city/map/city_name")
+getWidth = requests.get(f"{BASE_URL}/city/map/width")
+getHeight = requests.get(f"{BASE_URL}/city/map/height")
+getLegend = requests.get(f"{BASE_URL}/city/map/legend")
+getTiles = requests.get(f"{BASE_URL}/city/map/tiles")
+getGoal = requests.get(f"{BASE_URL}/city/map/goal")
+getMaxTime = requests.get(f"{BASE_URL}/city/map/max_time")
+getStreet = requests.get(f"{BASE_URL}/city/map/legend/C")
+getBuilding = requests.get(f"{BASE_URL}/city/map/legend/B")
+getParks = requests.get(f"{BASE_URL}/city/map/legend/P")
+getJobs = requests.get(f"{BASE_URL}/city/jobs/data")
+getWeather = requests.get(f"{BASE_URL}/city/weather?city=TigerCity&mode=seed")
+getWeatherConditions = (f"{getWeather}/conditions")
+getWeatherTransition = (f"{getWeather}/transition")

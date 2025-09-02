55d9d909db35080a32681230ba3bdc88ad874634
Tu Nombre <tuemail@example.com>
2025-09-01 20:13:24 -0600

    metodo calcular score final

--- Puntaje.py
+++ Puntaje.py
@@ -3,5 +3,11 @@ class Puntaje:
         self.score = score
         self.nombre = nombre
 
+    def calcular_score_final(self, suma_pagos, pay_mult, bonus_tiempo=0, penalizaciones=0):
+        score_base = suma_pagos * pay_mult
+        self.score = score_base + bonus_tiempo - penalizaciones
+        return self.score
+#el suma pagos seria ingresos de repartidor
+
     def __repr__(self):
         return f"{self.nombre}: {self.score}"
